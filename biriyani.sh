5f02405304c27d0afebe141c2045a2e5f4e351d0
“soumya” <bhosale.soumya@gmail.com>
2022-11-16 16:16:57 +0000

    version1.2

---
+++ biriyani.sh
@@ -0,0 +1,1 @@
+echo"Cooking made delightfully simple with Maggi Magic Cubes Chicken and everyday ingredients. Let’s make everyday dishes taste Magical with Maggi Magic Cubes Chicken! Veg Fried Rice Recipe. Chicken Fried Rice Recipe. Chicken Manchurian Recipe"
---
+++ egg.sh
@@ -0,0 +1,1 @@
+echo"Egg recipes - Collection of 52 anda recipes. Includes curries, snacks, rice , sandwiches and many more made with egg as the main ingredient"
