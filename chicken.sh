8a46ca6d31341c37de113b176897eb6c92ff6b92
souaccount <118281937+souaccount@users.noreply.github.com>
2022-11-28 18:25:09 +0530

    Update chicken.sh

--- chicken.sh
+++ chicken.sh
@@ -1,1 +1,2 @@
 echo "Indian chicken recipes - A unique collection of 66 simple Indian chicken recipes. Restaurant style starters, appetizers, chicken curry, biryani varieties"
+echo "tirgger in jenkins output" 
